359b0a1453c567b3546024740900cb4a634858c2
Jordan Conklin <jordan@caffeinated.org>
2025-03-10 15:55:00 -0400

    added limit setter with dropdown and next disables once reach page limit

--- react/pagination/app/page.tsx
+++ react/pagination/app/page.tsx
@@ -10,8 +10,9 @@ export default function Home() {
   const [logs, setLogs] = useState<Log[]>([]);
   const [page, setPage] = useState(1);
   const [searchInput, setSearchInput] = useState("")
+  const [limit, setLimit] = useState(10)
 
-  const limit = 10;
+  let numPages = Math.floor(100 / limit)
   
   useEffect(() => {
     async function fetchLogs() {
@@ -33,21 +34,37 @@ export default function Home() {
     };
 
     fetchLogs()
-  }, [page]); // Dependency array, effect runs when 'page' variable changes
+  }, [page, limit]); // Dependency array, effect runs when 'page' variable changes
 
   const fileredLogs = logs.filter((log) =>
     log.title.toLowerCase().includes(searchInput.toLowerCase())
   )
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-black">
       <div className="text-2xl font-bold">Pagination logs</div>
-      <input
-        type="text"
-        className="px-4 py-2"
-        placeholder="Search logs..."
-        value={searchInput}
-        onChange={(e) => setSearchInput(e.target.value)}
-      />
+      <div className="px-2">
+        {/* Search bar */}
+        <input
+          type="text"
+          className="px-4 py-2 border"
+          placeholder="Search logs..."
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+        {/* Limit selection dropdown */}
+        <select
+          className="px-4 py-2 border bg-gray-700"
+          value={limit}
+          onChange={(e) => setLimit(Number(e.target.value))}
+        >
+          {[...Array(11).keys()].map((_limit) => (
+            <option key={_limit} value={_limit + 5}>
+              {_limit + 5} per page
+            </option>
+          ))}
+        </select>
+      </div>
       <ul className="px-1 py-1">
         {fileredLogs.map((log) => (
           <li key={log.id} className="px-2 py-2">
@@ -56,6 +73,7 @@ export default function Home() {
         ))}
       </ul>
       <div className="flex gap-4">
+        {/* Previous button */}
         <button
           className="px-4 py-2 border bg-red-700 disabled:bg-gray-700"
           disabled={page === 1}
@@ -63,8 +81,10 @@ export default function Home() {
         >
           Previous
         </button>
+        {/* Next button */}
         <button
-          className="px-4 py-2 border bg-green-700"
+          className="px-4 py-2 border bg-green-700 disabled:bg-gray-700"
+          disabled={page >= numPages}
           onClick={() => setPage(page + 1)}
         >
           Next
@@ -79,7 +99,9 @@ export default function Home() {
 1
 
   pagination logs
-  || search ||
+  | search | |setLimit|
+  - sur       
+  - su
 
   -dfgsdf-
   -sdf-
