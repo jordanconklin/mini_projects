822b39fc271b69c0fe99373e22703fe2e9dc0d81
Jordan Conklin <jordan@caffeinated.org>
2025-03-10 00:40:20 -0400

    first commit for mini projects

---
+++ react/pagination/app/page.tsx
@@ -0,0 +1,92 @@
+"use client";
+import {useEffect, useState} from "react";
+
+interface Log {
+  id: number;
+  title: string;
+}
+
+export default function Home() {
+  const [logs, setLogs] = useState<Log[]>([]);
+  const [page, setPage] = useState(1);
+  const [searchInput, setSearchInput] = useState("")
+
+  const limit = 10;
+  
+  useEffect(() => {
+    async function fetchLogs() {
+      try {
+        console.log("Obtaining log data...")
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${limit}`);
+      
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json()
+        console.log(data);
+
+        setLogs(data) // update state with fetched variable
+      } catch(error) {
+        console.error("An error occurred: ", error)
+      }
+    };
+
+    fetchLogs()
+  }, [page]); // Dependency array, effect runs when 'page' variable changes
+
+  const fileredLogs = logs.filter((log) =>
+    log.title.toLowerCase().includes(searchInput.toLowerCase())
+  )
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-black">
+      <div className="text-2xl font-bold">Pagination logs</div>
+      <input
+        type="text"
+        className="px-4 py-2"
+        placeholder="Search logs..."
+        value={searchInput}
+        onChange={(e) => setSearchInput(e.target.value)}
+      />
+      <ul className="px-1 py-1">
+        {fileredLogs.map((log) => (
+          <li key={log.id} className="px-2 py-2">
+            {log.title}
+          </li>
+        ))}
+      </ul>
+      <div className="flex gap-4">
+        <button
+          className="px-4 py-2 border bg-red-700 disabled:bg-gray-700"
+          disabled={page === 1}
+          onClick={() => setPage(page - 1)}
+        >
+          Previous
+        </button>
+        <button
+          className="px-4 py-2 border bg-green-700"
+          onClick={() => setPage(page + 1)}
+        >
+          Next
+        </button>
+      </div>
+      
+    </div>
+  )
+}
+
+/*
+1
+
+  pagination logs
+  || search ||
+
+  -dfgsdf-
+  -sdf-
+  -
+  -
+
+  prev  next
+
+
+*/
